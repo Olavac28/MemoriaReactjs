8d27f910ecbb8255eacd8d88e54347643e1349e8
Hans <hans28h@gmail.com>
2025-09-26 11:45:58 -0300

    ahhhhh

---
+++ projeto1/src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <img src={reactLogo} alt='Reactlogo' />
+      <img src='/visite.svg' alt='visit logo' />
+    </div>
+  )
+}
+
+export default App
