facf61986449c769ea4035e60d57c017ddc677d8
Hans Carlos Cunha <hans@192.168.0.16>
2026-05-04 20:52:53 -0300

    primeira modificação

--- HelloWorld/src/main.jsx
+++ HelloWorld/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.jsx' //importa o App.jsx
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render( //renderize isso na #root do index.html
   <StrictMode>
-    <App />
+    <App /> {/*renderiza o componente App.jsx*/}
   </StrictMode>,
 )
