848d56d28ed658aaa897ccfdfc096c27427c906c
Hans <hans28h@gmail.com>
2026-05-06 00:22:44 -0300

    state

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 function App() {
-  const mensagem = 'Olá Mundo' //uso do jsx
+  //    [nome da variável, nome da Função](estado inicial da variável)
+  const [mensagem, setMensagem] = useState('Olá Mundo'); //state
+
   return ( //nn pode retornar mais de 1 elemento
-    <h1>{mensagem}</h1> //assim funciona o uso de variáveis
+    <div>
+      <h1>{mensagem}</h1> {/*assim funciona o uso de variáveis*/}
+      <button onClick={() => { {/*função de clique*/}
+        setMensagem('Fui Clickado'); {/*função que muda o state*/}
+      }}>Clique para mudar a menssagem</button>
+    </div>
   );
 }
 
